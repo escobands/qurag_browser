7cacce87c1b2acdb4a541c5d8365520d1f3df5d6
Francisco <plfrancom@icloud.com>
2025-05-10 15:27:53 +0200

    Qurag 5 stable co ja tu mialem napisac

---
+++ pak2_Qurag5.0/Qurag_Browser/Qurag.Designer.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Microsoft.Web.WebView2.WinForms;
+
+namespace Qurag_Browser
+{
+    partial class Qurag
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Qurag));
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.buttonVisit = new System.Windows.Forms.Button();
+            this.webView2Main = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonFwd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView2Main)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(74, 11);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(862, 20);
+            this.textBoxURL.TabIndex = 0;
+            this.textBoxURL.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxURL_KeyDown);
+            // 
+            // buttonVisit
+            // 
+            this.buttonVisit.Location = new System.Drawing.Point(942, 11);
+            this.buttonVisit.Name = "buttonVisit";
+            this.buttonVisit.Size = new System.Drawing.Size(97, 20);
+            this.buttonVisit.TabIndex = 1;
+            this.buttonVisit.Text = "Visit";
+            this.buttonVisit.UseVisualStyleBackColor = true;
+            this.buttonVisit.Click += new System.EventHandler(this.buttonVisit_Click);
+            // 
+            // webView2Main
+            // 
+            this.webView2Main.AllowExternalDrop = true;
+            this.webView2Main.CreationProperties = null;
+            this.webView2Main.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView2Main.Location = new System.Drawing.Point(0, 0);
+            this.webView2Main.Name = "webView2Main";
+            this.webView2Main.Size = new System.Drawing.Size(0, 0);
+            this.webView2Main.TabIndex = 0;
+            this.webView2Main.ZoomFactor = 1D;
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.webView21.CreationProperties = null;
+            this.webView21.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.webView21.Location = new System.Drawing.Point(13, 40);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(1477, 785);
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(13, 11);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(27, 20);
+            this.buttonBack.TabIndex = 2;
+            this.buttonBack.Text = "<-";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.BackButtonCL);
+            // 
+            // buttonFwd
+            // 
+            this.buttonFwd.Location = new System.Drawing.Point(41, 11);
+            this.buttonFwd.Name = "buttonFwd";
+            this.buttonFwd.Size = new System.Drawing.Size(27, 20);
+            this.buttonFwd.TabIndex = 3;
+            this.buttonFwd.Text = "->";
+            this.buttonFwd.UseVisualStyleBackColor = true;
+            this.buttonFwd.Click += new System.EventHandler(this.FwdButtonCL);
+            // 
+            // Qurag
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1502, 837);
+            this.Controls.Add(this.buttonFwd);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.buttonVisit);
+            this.Controls.Add(this.textBoxURL);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Qurag";
+            this.Text = "Qurag 5.0 (Stable)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView2Main)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private WebView2 webView2Main;
+
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Button buttonVisit;
+        private WebView2 webView21;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonFwd;
+    }
+}
---
+++ pak2_Qurag5.0/Qurag_Browser/Qurag.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Microsoft.Web.WebView2.WinForms;
+
+namespace Qurag_Browser
+{
+    public partial class Qurag : Form
+    {
+
+        public Qurag()
+        {
+            InitializeComponent();
+            webView2Main = new WebView2();
+            webView2Main.Dock = DockStyle.Fill;
+            this.Controls.Add(webView2Main);
+            this.Load += Form1_Load;
+            
+           
+        }
+            
+            
+
+        private void buttonVisit_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxURL.Text))
+            {
+                try
+                {
+                    webView2Main.CoreWebView2.Navigate(textBoxURL.Text);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Navigation has failed: " + ex.Message, "Qurag Browser", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Tworzymy WebView cz2
+                webView2Main.Dock = DockStyle.None;
+                webView2Main.Location = new Point(0, 50);
+                webView2Main.Size = new Size(this.ClientSize.Width, this.ClientSize.Height - 50);
+                webView2Main.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+                this.Controls.Add(webView2Main);
+                webView2Main.BringToFront();
+
+                // Inicjalizacja WebView i update url, jeszcze daj tam back/fwd
+                await webView2Main.EnsureCoreWebView2Async(null);
+                webView2Main.CoreWebView2.NavigationCompleted += (s, args) =>
+                {
+                    textBoxURL.Text = webView2Main.Source.ToString();
+                };
+
+                await webView2Main.EnsureCoreWebView2Async(null);
+                webView2Main.CoreWebView2.HistoryChanged += (s, args) =>
+                {
+                    buttonBack.Enabled = webView2Main.CanGoBack;
+                    buttonFwd.Enabled = webView2Main.CanGoForward;
+                };
+
+                // Strona poczatkowa
+                webView2Main.CoreWebView2.Navigate("about:blank");
+
+                // Eventy dla myszy
+                InitializeWebView2Events();
+
+                // Fokus
+                webView2Main.Focus();
+
+                // Lepiej fokus sie lapie
+                webView2Main.TabStop = true;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problem initializing engine: " + ex.Message);
+            }
+
+           
+        }
+
+        private void BackButtonCL(object sender, EventArgs e)
+        {
+            if (webView2Main.CanGoBack)
+                webView2Main.GoBack();
+        }
+
+        private void FwdButtonCL(object sender, EventArgs e)
+        {
+            if (webView2Main.CanGoForward)
+                webView2Main.GoForward();
+        }
+
+        private void textBoxURL_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                buttonVisit_Click(sender, e);
+            }
+        }
+        // W Konstruktorze lub w Load formy:
+        private void InitializeWebView2Events()
+        {
+            // Rejestracja eventów na WebView2
+            // Uzywano do debugowania z mysza
+            // Chyba da sie usunac
+            // Nie chce mi sie
+            webView2Main.MouseDown += WebView2Main_MouseDown;
+            webView2Main.MouseClick += WebView2Main_MouseClick;
+            webView2Main.MouseWheel += WebView2Main_MouseWheel;
+        }
+
+        private void WebView2Main_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Tutaj nic
+        }
+
+        private void WebView2Main_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Tez nic
+        }
+
+        private void WebView2Main_MouseWheel(object sender, MouseEventArgs e)
+        {
+            // Nic
+        }
+
+        private void Qurag_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            webView2Main.Dispose();
+        }
+
+        }
+    }
+
